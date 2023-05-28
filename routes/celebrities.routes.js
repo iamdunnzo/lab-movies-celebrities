9e926dddd9305bf457fe89aade9fac7abe868575
Courtney Dunn <courtney.a.dunn@gmail.com>
2023-05-27 18:45:19 -0700

    iteration 3 and 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,4 +2,37 @@ const router = require("express").Router();
 
 // Define your celebrity routes here
 
+router.get("/create", (req, res) => {
+    res.render("celebrities/new-celebrity");
+  });
+
+  router.post("/create", (req, res) => {
+    const { name, occupation, catchPhrase } = req.body;
+  
+    const celebrity = new Celebrity({
+      name,
+      occupation,
+      catchPhrase,
+    });
+  
+    celebrity.save()
+      .then(() => {
+        res.redirect("/celebrities");
+      })
+      .catch((error) => {
+        res.render("celebrities/new-celebrity", { error });
+      });
+  });
+  
+  router.get("/", (req, res) => {
+    Celebrity.find()
+      .then((celebrities) => {
+        res.render("celebrities/celebrities", { celebrities });
+      })
+      .catch((error) => {
+        // Handle the error
+      });
+  });
+  
+
 module.exports = router;
