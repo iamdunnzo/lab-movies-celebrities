1577075b5443fc1862c8e610145b5e2bf418dafa
Courtney Dunn <courtney.a.dunn@gmail.com>
2023-05-27 19:10:43 -0700

    iteration 5 and 6 completed

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,4 +2,32 @@ const router = require("express").Router();
 
 // Define your celebrity routes here
 
+router.get("/create", async (req, res) => {
+    try {
+      const celebrities = await Celebrity.find();
+      res.render("movies/new-movie", { celebrities });
+    } catch (error) {
+      // Handle the error
+    }
+  });
+  
+  router.post("/create", async (req, res) => {
+    const { title, genre, plot, cast } = req.body;
+  
+    try {
+      const movie = new Movie({
+        title,
+        genre,
+        plot,
+        cast,
+      });
+  
+      await movie.save();
+  
+      res.redirect("/movies");
+    } catch (error) {
+      // Handle the error
+    }
+  });
+  
 module.exports = router;
